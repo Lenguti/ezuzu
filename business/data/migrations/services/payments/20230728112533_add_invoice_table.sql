da52221fb98297c2e17ca8ede4410b4fa647f808
Lenard Gutierrez <lenguti@gmail.com>
2023-07-28 11:43:07 -0400

    add payments tables, remove tennant portal

---
+++ business/data/migrations/services/payments/20230728112533_add_invoice_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE invoices (
+    id uuid NOT NULL,
+    property_id uuid NOT NULL,
+    tennant_id uuid NOT NULL,
+    amount float,
+    due_date int,
+    created_at int,
+    updated_at int,
+    PRIMARY KEY (id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE invoices;
+-- +goose StatementEnd
---
+++ business/data/migrations/services/payments/20230728114014_add_payments_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE payments (
+    id uuid NOT NULL,
+    invoice_id uuid NOT NULL,
+    tennant_id uuid NOT NULL,
+    amount float,
+    created_at int,
+    updated_at int,
+    PRIMARY KEY (id),
+    FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE payments;
+-- +goose StatementEnd
